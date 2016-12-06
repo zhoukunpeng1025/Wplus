5ff609b59a580e0c697f7cb70e4d2722efd2d5d5
Zhou Kunpeng <1130797520@qq.com>
2016-12-06 08:49:10 +0800

    add Thinkphp File

---
+++ ThinkWplus/Application/Common/Conf/config.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+	'DB_TYPE' => 'mysql',
+	'DB_HOST' => '123.207.144.161',
+	'DB_NAME' => 'wplus',
+	'DB_USER' => 'admin',
+	'DB_PWDD' => '123456',
+	'DB_PORT' => '3306',
+	'URL_MODEL' => 1,
+);
