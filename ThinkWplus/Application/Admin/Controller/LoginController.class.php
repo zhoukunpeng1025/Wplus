d89a22abcf15ef7d7ce5c4e9910f472fbea69bf1
jialin <594619213@qq.com>
2016-12-12 14:06:21 +0800

    登录页

---
+++ ThinkWplus/Application/Admin/Controller/LoginController.class.php
@@ -0,0 +1,34 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+class LoginController extends Controller {
+    public function login(){
+    	if (IS_POST) {
+			$adminUsersModel = M('adminuser');
+			$condition = array(
+				"adminname" => I("post.adminname"),
+				"password" => I("post.password")
+			);
+			$result = $adminUsersModel->where($condition)->count();
+			$id = $adminUsersModel->field('id')->where($condition)->select();
+			// var_dump($result);
+			if ($result > 0) {
+				$_SESSION['adminname'] = I("post.adminname");
+				$_SESSION['userid'] = $id[0]["id"];
+				// ？？用数组设定session不生效
+				// session(array(
+				// 	"username" => I("post.username"),
+				// 	"userid" => $id[0]["id"]
+				// 	));
+				// var_dump($_SESSION);
+				$this->success("登录成功", U("Index/index"));
+			}
+			else{
+				$this->error("用户名或密码不正确");
+			}
+		}
+		else{
+			$this->display();
+		}
+    }
+}
