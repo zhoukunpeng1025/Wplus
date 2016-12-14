c35c5ee14a6e999d26534f994773e15a0419270c
luxiaojia <490423467@qq.com>
2016-12-14 08:44:34 +0800

    手机验证功能

--- ThinkWplus/Application/Home/Controller/SignupController.class.php
+++ ThinkWplus/Application/Home/Controller/SignupController.class.php
@@ -2,33 +2,87 @@
 namespace Home\Controller;
 use Think\Controller;
 class SignupController extends Controller {
-    public function signup1(){
+	public function signin(){
+		$this->display();
+	}
+
+    public function signup(){
 		$this->display();
 	}
 
     public function sendMsg(){
-		$appkey = "23564641";//你的App key
-		$secret = "00816798fbc0c004f6b35851a56b236a";//你的App Secret: 
-		vendor('Dayu.top.TopClient');
-		vendor('Dayu.top.ResultSet');
-		vendor('Dayu.top.RequestCheckUtil');
-		vendor('Dayu.top.TopLogger');
-		vendor('Dayu.top.request.AlibabaAliqinFcSmsNumSendRequest'); 
-		$c = new TopClient;
-		$c->appkey = $appkey;
-		$c->secretKey = $secret;
-		$req = new AlibabaAliqinFcSmsNumSendRequest;
-		$req->setExtend("");
-		$req->setSmsType("normal");
-		$req->setSmsFreeSignName("味+");
-		session_start();
-		$verifycode = strval(rand(100000,999999));
-		$_SESSION['verifycode'] = $verifycode;
-		$req->setSmsParam(json_encode($smsParams));
-		$phonenum = I("post.phonenum");
-		$_SESSION['phonenum'] = $phonenum;
-		$req->setRecNum($phonenum);
-		$req->setSmsTemplateCode("SMS_33855075");
-		$resp = $c->execute($req);
-		var_dump($resp);
+    	// 此功能需要开启php_curl.dll扩展
+		
+		if(IS_POST){
+			$phonenum = I("post.phonenum");
+			$verifycode = strval(rand(100000,999999)); // 随机一个六位验证码
+			$_SESSION['phonenum'] = $phonenum;
+			$_SESSION['verifycode'] = $verifycode;
+			// dump($_SESSION);
+
+			// $appkey = "23564641";
+			// $secret = "00816798fbc0c004f6b35851a56b236a";
+			// vendor('Dayu.top.TopClient');
+			// vendor('Dayu.top.ResultSet');
+			// vendor('Dayu.top.RequestCheckUtil');
+			// vendor('Dayu.top.TopLogger');
+			// vendor('Dayu.top.request.AlibabaAliqinFcSmsNumSendRequest'); 
+			// $c = new \TopClient;
+			// $c->appkey = $appkey;
+			// $c->secretKey = $secret;
+			// $req = new \AlibabaAliqinFcSmsNumSendRequest;
+			// $req->setExtend("");
+			// $req->setSmsType("normal");
+			// $req->setSmsFreeSignName("味+");
+			// $req->setSmsParam("{number:'$verifycode'}");
+			// $req->setRecNum("$phonenum");
+			// $req->setSmsTemplateCode("SMS_33855075");
+			// $resp = $c->execute($req);
+
+			// dump($resp->result->success);
+			// if($resp->result->success == "true") {
+			if(true){
+				// 发送成功
+				$this->redirect("Signup/verify");
+			}
+		}
+	}
+	public function verify(){
+		$this->display();
+	}
+
+	public function doVerify(){
+		if(IS_POST) {		
+			if(I("post.verifycode") == I("session.verifycode")){
+				// 验证成功
+				$this->redirect("Signup/setting");
+			}
+			else {
+				// 验证失败，toast提示
+				// echo "<script>alert('输入错误，请重新输入')</script>";
+				// return false;
+			}
+		}
+	}
+
+	public function setting(){
+		$this->display();
+	}
+
+	public function doSet(){
+		if (IS_POST) {
+			$username = I("post.username");
+			$password = I("post.password");
+			$_SESSION['username'] = $username;
+			$_SESSION['password'] = $password;
+
+			$data = $_POST;
+			$data['phonenum'] = I("session.phonenum");
+
+			$userModel = M("user");
+			if($userModel->add($data)) {
+				$this->redirect("Index/index");
+			}
+		}
+	}
 }
