c5d0ca4d5e59116562f68946f0cc6b33c45cd465
wuyiwen0 <1317281881@qq.com>
2016-12-14 20:57:26 +0800

    Thinkphp前台菜谱相关页面

---
+++ ThinkWplus/Application/Home/Controller/MenuController.class.php
@@ -0,0 +1,16 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+
+class MenuController extends Controller {
+	//assortment页动态获取
+	public function assortment(){
+
+	}
+	// lists页动态获取
+	public function lists(){
+		
+	}
+
+}
+?>
