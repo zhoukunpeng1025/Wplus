16c21472763ef07fa6811a465a7cee9c3e0e0aaf
wuyiwen0 <1317281881@qq.com>
2016-12-15 10:02:24 +0800

    添加

--- ThinkWplus/Application/Home/Controller/MenuController.class.php
+++ ThinkWplus/Application/Home/Controller/MenuController.class.php
@@ -3,6 +3,10 @@ namespace Home\Controller;
 use Think\Controller;
 
 class MenuController extends Controller {
+	// 添加菜谱add页
+	public function add(){
+		$this->display();
+	}
 	//assortment页动态获取
 	public function assortment(){
 		$this->display();
@@ -11,6 +15,13 @@ class MenuController extends Controller {
 	public function lists(){
 		$this->display();
 	}
-
+	//content页
+	public function content(){
+		$this->display();
+	}
+	//steps页
+	public function steps(){
+		$this->display();
+	}
 }
 ?>
