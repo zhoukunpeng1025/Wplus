4ce36421d141350cd3f19d251902dc2f8e8ac4cd
luxiaojia <490423467@qq.com>
2016-12-15 09:24:53 +0800

    直播间

---
+++ ThinkWplus/Application/Home/Controller/VideoController.class.php
@@ -0,0 +1,14 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class VideoController extends Controller {
+	public function lists(){
+		$this->display();
+	}
+	public function content(){
+		$this->display();
+	}
+	public function start(){
+		$this->display();
+	}
+}
