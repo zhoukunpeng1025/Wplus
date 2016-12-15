c9e2a294da88f8240735d751d0c950df28ecb011
luxiaojia <490423467@qq.com>
2016-12-15 12:03:43 +0800

    我

---
+++ ThinkWplus/Application/Home/Controller/MeController.class.php
@@ -0,0 +1,12 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class MeController extends Controller {
+    public function index(){
+        $this->display();
+    }
+
+    public function setting(){
+        $this->display();
+    }
+}
