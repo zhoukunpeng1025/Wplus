ff88c97ca4342ed09b96444c26aa03f7fd920220
wuyiwen0 <1317281881@qq.com>
2016-12-15 11:45:34 +0800

    menu-content页动态获取

--- ThinkWplus/Application/Home/Controller/MenuController.class.php
+++ ThinkWplus/Application/Home/Controller/MenuController.class.php
@@ -17,6 +17,20 @@ class MenuController extends Controller {
 	}
 	//content页
 	public function content(){
+		$recipeModel = M('recipe');
+		$recipe = $recipeModel->where("id=1")->find();//能获取当前数据
+
+		// 将字符串转为数组
+		$recipe["foodname"] = explode("&", $recipe["foodname"]);
+		$recipe["foodnum"] = explode("&", $recipe["foodnum"]);
+		// $recipe[$i]["food"] = array("foodname"=>$foodname, "foodnum"=>$foodnum);
+		$recipe["step"] = explode("&", $recipe["step"]);
+		$recipe["img"] = explode("&", $recipe["img"]);
+		$recipe["clocktime"] = explode("&", $recipe["clocktime"]);
+		$recipe["clocknum"] = explode("&", $recipe["clocknum"]);
+		// }
+		$this->assign("recipe",$recipe);
+
 		$this->display();
 	}
 	//steps页
