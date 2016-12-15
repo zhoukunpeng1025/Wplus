0b8dca0103043e358e5252611b6162764ab08628
luxiaojia <490423467@qq.com>
2016-12-15 11:04:52 +0800

    主页判断是否登录

--- ThinkWplus/Application/Home/Controller/IndexController.class.php
+++ ThinkWplus/Application/Home/Controller/IndexController.class.php
@@ -2,6 +2,12 @@
 namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
+	public function __construct() {
+		parent::__construct();
+		if (!isLogin()) {
+			$this->redirect("Signup/signin");
+		}
+	}
     public function index(){
     	$Model = M();
     	$article = $Model->table("article a,user b")->where("a.authorid = b.id")->limit(10)->select();
