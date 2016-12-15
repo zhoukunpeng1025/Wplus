56557b2703f6db6b3aa236edf2d537234be459d0
luxiaojia <490423467@qq.com>
2016-12-15 10:00:46 +0800

    附近

---
+++ ThinkWplus/Application/Home/Controller/NearbyController.class.php
@@ -0,0 +1,17 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class NearbyController extends Controller {
+	public function lists(){
+		$this->display();
+	}
+	public function content(){
+		$this->display();
+	}
+	public function dynamicedit(){
+		$this->display();
+	}
+	public function essayedit(){
+		$this->display();
+	}
+}
