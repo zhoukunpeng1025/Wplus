4b8bf067c4d07bed862a192824098763503680b8
luxiaojia <490423467@qq.com>
2016-12-18 23:55:08 +0800

    上传图片

--- ThinkWplus/Application/Admin/Controller/RecipeController.class.php
+++ ThinkWplus/Application/Admin/Controller/RecipeController.class.php
@@ -13,9 +13,9 @@ class RecipeController extends Controller {
 			$recipe[$i]["foodnum"] = explode("&", $recipe[$i]["foodnum"]);
 			// $recipe[$i]["food"] = array("foodname"=>$foodname, "foodnum"=>$foodnum);
 			$recipe[$i]["step"] = explode("&", $recipe[$i]["step"]);
-			$recipe[$i]["img"] = explode("&", $recipe[$i]["img"]);
 			$recipe[$i]["clocktime"] = explode("&", $recipe[$i]["clocktime"]);
 			$recipe[$i]["clocknum"] = explode("&", $recipe[$i]["clocknum"]);
+			$recipe[$i]["img"] = explode("&", $recipe[$i]["img"]);
 		}
 
 		// dump($recipe);
@@ -34,26 +34,46 @@ class RecipeController extends Controller {
     	if (!IS_POST) {
 			exit("bad request!");
 		}
-		$data = $_POST;
 
-		// 将数组转成字符串
-		$data["foodname"] = implode("&", $data["foodname"]);
-		$data["foodnum"] = implode("&", $data["foodnum"]);
-		$data["step"] = implode("&", $data["step"]);
-		$data["clocknum"] = implode("&", $data["clocknum"]);
-		$data["clocktime"] = implode("&", $data["clocktime"]);
-		// dump($data);
+		$upload = new \Think\Upload();// 实例化上传类
+		$upload->maxSize=3145728 ;// 设置附件上传大小
+		$upload->exts=array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+		$upload->rootPath = THINK_PATH; // 设置附件上传根目录
+		$upload->savePath ='../../../Public/uploads/'; // 设置附件上传（子）目录
+		// 上传文件 
+		$info = $upload->upload();
+		if(!$info) {// 上传错误提示错误信息
+			$this->error($upload->getError());
+		}
+		else{
+			// 设置存储路径
+			for($i = 0; $i<count($info); $i++){
+				$savepath[$i] = $info[$i]["savepath"].$info[$i]["savename"];
+			}
 
-		// $recipeModel = M("recipe");
-		// if (!$recipeModel->create()) {
-		// 	$this->error($recipeModel->getError());
-		// }
-		// if ($recipeModel->add($data)) {
-		// 	$this->success("添加成功！", U("lists"));
-		// }
-		// else {
-		// 	$this->error("添加失败！");
-		// }
+			$data = $_POST;
+			// 将数组转成字符串
+			$data["foodname"] = implode("&", $data["foodname"]);
+			$data["foodnum"] = implode("&", $data["foodnum"]);
+			$data["step"] = implode("&", $data["step"]);
+			$data["clocknum"] = implode("&", $data["clocknum"]);
+			$data["clocktime"] = implode("&", $data["clocktime"]);
+			// $data["img"] = implode("&", $savepath);
+			// dump($data);
+
+			$recipeModel = M("recipe");
+			if (!$recipeModel->create()) {
+				$this->error($recipeModel->getError());
+			}
+			if ($recipeModel->add($data)) {
+				$this->success("添加成功！", U("lists"));
+			}
+			else {
+				$this->error("添加失败！");
+			}
+		}
+
+		
     }
 
     // 显示需要编辑的菜谱的信息
@@ -68,6 +88,7 @@ class RecipeController extends Controller {
 		$recipe[0]["step"] = explode("&", $recipe[0]["step"]);
 		$recipe[0]["clocktime"] = explode("&", $recipe[0]["clocktime"]);
 		$recipe[0]["clocknum"] = explode("&", $recipe[0]["clocknum"]);
+		$recipe[0]["img"] = explode("&", $recipe[0]["img"]);
 
 		// dump($recipe);
 		$this->assign('recipe', $recipe);
