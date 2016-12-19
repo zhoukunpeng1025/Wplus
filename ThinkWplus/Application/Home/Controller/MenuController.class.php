a263489d21f2e255114b2bb10f052e9f7c150e5b
luxiaojia <490423467@qq.com>
2016-12-19 17:29:01 +0800

    动态获取菜谱列表

--- ThinkWplus/Application/Home/Controller/MenuController.class.php
+++ ThinkWplus/Application/Home/Controller/MenuController.class.php
@@ -14,22 +14,40 @@ class MenuController extends Controller {
 		$this->assign("menu", $menu);
 		$this->display();
 	}
+
 	// lists页动态获取
-	public function lists(){
+	public function lists($tagid){
+		// dump($tagid)
+		$menuModel = M("menu");
+		$menu = $menuModel->where("id=$tagid")->select();
+		$this->assign("menu", $menu);
+
+		$recipeModel = M("recipe");
+		$recipe = $recipeModel->where("recipetagid = $tagid")->field("id,recipename,img")->select();
+
+		for($i = 0; $i < count($recipe); $i++) {
+			// 获取步骤中最后一张图
+			$pos = strripos($recipe[$i]['img'], "uploads");
+			$recipe[$i]["img"] = substr($recipe[$i]["img"], $pos);
+		}
+		// dump($recipe);
+
+		$this->assign("recipe", $recipe);
 		$this->display();
 	}
 	//content页
 	public function content(){
+		$id = I("id");
 		$recipeModel = M('recipe');
-		$recipe = $recipeModel->where("id=1")->find();//能获取当前数据
+		$recipe = $recipeModel->where("id=$id")->find();//能获取当前数据
 
 		// 将字符串转为数组
 		$recipe["foodname"] = explode("&", $recipe["foodname"]);
 		$recipe["foodnum"] = explode("&", $recipe["foodnum"]);
 		$recipe["step"] = explode("&", $recipe["step"]);
-		$recipe["img"] = explode("&", $recipe["img"]);
 		$recipe["clocktime"] = explode("&", $recipe["clocktime"]);
 		$recipe["clocknum"] = explode("&", $recipe["clocknum"]);
+		$recipe["img"] = explode("&", $recipe["img"]);
 		
 		$this->assign("recipe",$recipe);
 		$this->display();
