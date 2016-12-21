0233cb402fd2d3c851bded6d63c81dc8fa52216c
liulinhui <1375388499@qq.com>
2016-12-21 20:07:30 +0800

    修改文章管理

--- ThinkWplus/Application/Admin/Controller/WorkController.class.php
+++ ThinkWplus/Application/Admin/Controller/WorkController.class.php
@@ -11,8 +11,29 @@ class WorkController extends Controller{
       $this->assign("article", $article);
       $this->display();
 
+     
 
-    }
+   }
+   public function page(){
+        $User = M('article'); 
+        $count      = $User->count();// 查询满足要求的总记录数
+        $Page       = new \Think\Page($count,6);// 实例化分页类 传入总记录数和每页显示的记录数
+        $orderby['id']='ASC';
+
+        $image = $Model->order($orderby)->limit($Page->firstRow.','.$Page->listRows)->select();
+
+        $this->assign(article,$image);// 赋值数据集
+        $Page -> setConfig('first','首页');
+        $Page -> setConfig('last','共%TOTAL_PAGE%页');
+        $Page -> setConfig('prev','上一页');
+        $Page -> setConfig('next','下一页');
+        $Page -> setConfig('link','indexpagenumb');//pagenumb 会替换成页码
+        $Page -> setConfig('theme','%HEADER% %FIRST% %UP_PAGE% %LINK_PAGE% %DOWN_PAGE% %END%');
+        $show= $Page->show();
+        $this->assign('page',$show);// 赋值分页输出
+
+        $this->display(); // 输出模板
+   }
      //单条删除数据
      public function articleDelete(){
           $id=I('id');
@@ -39,10 +60,29 @@ class WorkController extends Controller{
     }
 
     public function imageLists(){
-      $imageModel = M("Image");
-      $image = $imageModel->select();
-      $this->assign("image", $image);
-      $this->display();
+      // $imageModel = M("Image");
+      // $image = $imageModel->select();
+      // $this->assign("image", $image);
+      // $this->display();
+
+       $User = M('Image'); 
+        $count      = $User->count();// 查询满足要求的总记录数
+        $Page       = new \Think\Page($count,6);// 实例化分页类 传入总记录数和每页显示的记录数
+        $orderby['id']='ASC';
+
+        $image = $User->order($orderby)->limit($Page->firstRow.','.$Page->listRows)->select();
+
+        $this->assign('image',$image);// 赋值数据集
+        $Page -> setConfig('first','首页');
+        $Page -> setConfig('last','共%TOTAL_PAGE%页');
+        $Page -> setConfig('prev','上一页');
+        $Page -> setConfig('next','下一页');
+        $Page -> setConfig('link','indexpagenumb');//pagenumb 会替换成页码
+        $Page -> setConfig('theme','%HEADER% %FIRST% %UP_PAGE% %LINK_PAGE% %DOWN_PAGE% %END%');
+        $show= $Page->show();
+        $this->assign('page',$show);// 赋值分页输出
+
+        $this->display(); // 输出模板
     }
     //单条删除
     public function imageDelete(){
@@ -72,10 +112,25 @@ class WorkController extends Controller{
     }
 
     public function videoLists(){
-      $videoModel = M("Video");
-      $video = $videoModel->select();
-      $this->assign("video", $video);
-      $this->display();
+
+      $User = M('Video'); 
+        $count      = $User->count();// 查询满足要求的总记录数
+        $Page       = new \Think\Page($count,8);// 实例化分页类 传入总记录数和每页显示的记录数
+        $orderby['id']='ASC';
+
+        $video = $User->order($orderby)->limit($Page->firstRow.','.$Page->listRows)->select();
+
+        $this->assign('video',$video);// 赋值数据集
+        $Page -> setConfig('first','首页');
+        $Page -> setConfig('last','共%TOTAL_PAGE%页');
+        $Page -> setConfig('prev','上一页');
+        $Page -> setConfig('next','下一页');
+        $Page -> setConfig('link','indexpagenumb');//pagenumb 会替换成页码
+        $Page -> setConfig('theme','%HEADER% %FIRST% %UP_PAGE% %LINK_PAGE% %DOWN_PAGE% %END%');
+        $show= $Page->show();
+        $this->assign('page',$show);// 赋值分页输出
+
+        $this->display(); // 输出模板
     }
     //单条删除
     public function videoDelete(){
@@ -105,7 +160,8 @@ class WorkController extends Controller{
     public function article(){
       $id = I("id");
       $articleModel = M("Article");
-      $article = $articleModel->where("id=$id")->select();
+      $article = $articleModel->join('user ON article.authorid = user.id');
+      $article = $article->where("article.id=$id")->select();
       $this->assign("article", $article);
       $this->display();
     }
