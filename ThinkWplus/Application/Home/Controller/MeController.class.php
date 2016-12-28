9eefb7c3b73beaa82d0d4d07e436953e97ba967e
luxiaojia <490423467@qq.com>
2016-12-28 14:38:02 +0800

    修改进行中的订单

--- ThinkWplus/Application/Home/Controller/MeController.class.php
+++ ThinkWplus/Application/Home/Controller/MeController.class.php
@@ -14,10 +14,14 @@ class MeController extends Controller {
            $unclaimed[$i]["ingredients"] = str_replace("&", "、", $unclaimed[$i]["ingredients"]);
         }
         // 已领取的订单
-        $inProgress = $Model->table("orderform a, user b")->where("a.makerid = b.id and a.makerid = $id and a.purchaserid <> $id")->limit(10)->select();
+        // 不显示自己发布领取的订单
+        // $inProgress = $Model->table("orderform a, user b, user c")->where("a.makerid = b.id and a.makerid = $id and a.purchaserid <> $id and a.purchaserid = c.id")->limit(10)->select();
+        // 显示自己发布领取的订单
+        $inProgress = $Model->table("orderform a, user b, user c")->where("a.makerid = b.id and a.makerid = $id and a.purchaserid = c.id")->limit(10)->select();
         for ($i=0; $i < count($inProgress) ; $i++) { 
            $inProgress[$i]["ingredients"] = str_replace("&", "、", $inProgress[$i]["ingredients"]);
         }
+        // dump($inProgress);
         
         $this->assign("unclaimed", $unclaimed);
         $this->assign("inProgress", $inProgress);
